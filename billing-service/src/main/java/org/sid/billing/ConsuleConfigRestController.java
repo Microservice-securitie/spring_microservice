db2a4a1ff2724c62a6bb7e8ad08d263696b0b954
othmanetozy <tozyothmane123@gmail.com>
2023-12-30 21:09:22 +0100

    Adding EndPoint to consule config  & fix error

---
+++ billing-service/src/main/java/org/sid/billing/ConsuleConfigRestController.java
@@ -0,0 +1,22 @@
+package org.sid.billing;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ConsuleConfigRestController {
+    @Value("${token.accesTokenTimeOut}")
+    private long accesTokenTimeOut;
+    @Value("${token.refreshTokenTimeOut}")
+    private long refreshTokenTimeOut;
+
+
+    @GetMapping("/myconfig")
+    public Map<String,Object> myConfig(){
+        return Map.of("accesTokenTimeOut",accesTokenTimeOut,"refreshTokenTimeOut",refreshTokenTimeOut);  //key.value
+    }
+
+}
